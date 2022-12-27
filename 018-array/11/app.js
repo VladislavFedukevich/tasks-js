8bd5ee5cfb32128eaeb28fcd0125993dbb7dba12
karmanchik567 <fedukevicvlad@gmail.com>
2022-12-27 23:19:49 +0300

    fix

--- 018-array/11/app.js
+++ 018-array/11/app.js
@@ -1,8 +1,8 @@
-const arr = [1, 2, 6, 'asfaf', 5, true, null, 'sfdsf'];
+const arr = [1, 2, 6, "asfaf", 5, true, null, "sfdsf"];
 const final_arr = [];
 
 for (let i = 0; i < arr.length; i++) {
-  if (typeof arr[i] === 'number') {
+  if (typeof arr[i] === "number") {
     final_arr.push(arr[i]);
   }
 }
