d1baa77b0914f3dd0708db5d963c2e090f4ab657
karmanchik567 <fedukevicvlad@gmail.com>
2023-01-03 20:37:06 +0300

    add 7

---
+++ 019-array-method/7/app.js
@@ -0,0 +1,11 @@
+const array = ['hschool', 'hschool_0', 'hschool_1'];
+
+let firstElement_1 = array[0];
+
+let lastElement_1 = array[array.length - 1];
+
+let firstElement_2 = array.shift();
+
+let lastElement_2 = array.pop();
+
+let [firstElement_3] = array;
