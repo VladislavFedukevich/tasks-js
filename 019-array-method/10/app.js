9e8411d5611049b456d4d32748d42e5157c1983a
karmanchik567 <fedukevicvlad@gmail.com>
2023-01-06 02:34:08 +0300

    10

---
+++ 019-array-method/10/app.js
@@ -0,0 +1,16 @@
+const n = 'Enter the length:';
+const arr = [];
+
+for (let i = 0; i < n; i++) {
+  arr.push(prompt('Enter the value'));
+}
+
+const result = arr.map(elem => {
+  if (elem % 2 === 0) {
+    return 'even';
+  } else {
+    return 'odd';
+  }
+})
+
+console.log(result);
