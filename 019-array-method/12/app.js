f3d9d927f6e00f293276d9bdf50237a202cbac42
karmanchik567 <fedukevicvlad@gmail.com>
2023-01-07 18:13:00 +0300

    12

--- 019-array-method/12/app.js
+++ 019-array-method/12/app.js
@@ -1,6 +1,21 @@
-const n = prompt("Enter the length:");
-const arr = [];
+// const n = prompt("Enter the length:");
+// const arr = [];
+const arr = ["ann", "school", "hschool", "borabora"];
+const final = [];
 
-for (let i = 0; i < n; i++) {
-  arr.push(prompt("Enter the value:"));
+for (let i = 0; i < arr.length; i++) {
+  // arr.push(prompt("Enter the value:"));
+  final.push(arr[i]);
 }
+
+let result = final.filter(elem => {
+  if (!isNaN(elem)) {
+    console.log('Error');
+  } else {
+    if (elem[0] === 'a' || elem[0] === 'h') {
+      return true;
+    }
+  }
+})
+
+console.log(result);
