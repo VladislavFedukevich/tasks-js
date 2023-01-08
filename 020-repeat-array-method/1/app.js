3ac051f8fd28ddc5432e7ac9410d5861545eaf48
karmanchik567 <fedukevicvlad@gmail.com>
2023-01-08 18:25:38 +0300

    1

---
+++ 020-repeat-array-method/1/app.js
@@ -0,0 +1,12 @@
+const n = prompt('Enter the length:');
+const arr = [];
+
+for (let i = 0; i < n; i++) {
+  arr.push(+prompt('Enter the value'));
+}
+
+let sum = arr.reduce(function(sum, elem) {
+  return sum + elem;
+}, 0)
+
+console.log(sum);
