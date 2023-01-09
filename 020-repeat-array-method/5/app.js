1120b251df66d3b140ff2124aeff00fee49b647c
karmanchik567 <fedukevicvlad@gmail.com>
2023-01-09 23:12:50 +0300

    5

--- 020-repeat-array-method/5/app.js
+++ 020-repeat-array-method/5/app.js
@@ -1,16 +1,21 @@
-const n = prompt('Enter the length');
+const n = prompt("Enter the array");
 const arr = [];
+const result = [];
 
 for (let i = 0; i < n; i++) {
   arr.push(prompt('Enter the value'));
 }
 
-let max = 0;
-let filtered = arr.filter(elem => {
-  if (elem > max) {
-    max = elem;
-    return max;
+const filtered = arr.filter((elem) => {
+  if (
+    elem === "a" ||
+    elem === "e" ||
+    elem === "u" ||
+    elem === "i" ||
+    elem === "o"
+  ) {
+    return true;
   }
-})
+});
 
-console.log(max);
+console.log(filtered);
