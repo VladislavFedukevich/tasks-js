e5c8ad57442fc1312d444153f5a63693d55da981
karmanchik567 <fedukevicvlad@gmail.com>
2023-01-17 21:38:23 +0300

    13

--- 023-context/13/app.js
+++ 023-context/13/app.js
@@ -1,1 +1,15 @@
-
+const generateSecurePassword = () => {
+  let password = "";
+  const possibleCharacters = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm0123456789!@#$%^&*()_-+";
+  const passwordLength = 19;
+  for (let i = 0; i < passwordLength; i++) {
+    if (i === 4 || i === 9 || i === 14) {
+      password += "-";
+    } else {
+      const randomIndex = Math.floor(Math.random() * possibleCharacters.length);
+      password += possibleCharacters[randomIndex];
+    }
+  }
+  return password;
+};
+console.log(generateSecurePassword());
