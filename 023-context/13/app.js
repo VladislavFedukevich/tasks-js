7c5efed192bdf6e1b345a901f30d6626e7ea15f9
karmanchik567 <fedukevicvlad@gmail.com>
2023-01-17 19:41:44 +0300

    clear

--- 023-context/13/app.js
+++ 023-context/13/app.js
@@ -1,15 +1,1 @@
-let string = 'change';
 
-const concatFunction = () => {
-  return function() {
-    string += ' changed';
-    console.log(string);
-  }
-}
-
-let changed = concatFunction();
-
-changed();
-changed();
-changed();
-changed();
