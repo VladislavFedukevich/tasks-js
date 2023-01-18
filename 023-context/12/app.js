137df3ef518b42e4b510e29e0eec643cd6bb6719
karmanchik567 <fedukevicvlad@gmail.com>
2023-01-18 23:29:05 +0300

    fix

--- 023-context/12/app.js
+++ 023-context/12/app.js
@@ -12,4 +12,4 @@ const obj = {
   }
 
   console.log(count);
-}(obj))
+})(obj);
--- 023-context/13/app.js
+++ 023-context/13/app.js
@@ -1,6 +1,7 @@
 const generateSecurePassword = () => {
   let password = "";
-  const possibleCharacters = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm0123456789!@#$%^&*()_-+";
+  const possibleCharacters =
+    "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm0123456789!@#$%^&*()_-+";
   const passwordLength = 19;
   for (let i = 0; i < passwordLength; i++) {
     if (i === 4 || i === 9 || i === 14) {
