757c60e4c660be631c81ca56c614909c65c4cb9a
karmanchik567 <fedukevicvlad@gmail.com>
2023-01-31 17:15:28 +0300

    fix-9

--- 026-JSON-Exception/9/app.js
+++ 026-JSON-Exception/9/app.js
@@ -1,25 +1,30 @@
 const staticArray = [5, 1, 2, 3, 0, 1, 5, 0, 2];
 
+const isNumberArray = (arr) => {
+  arr.forEach((elem) => {
+    if (isNaN(elem)) throw new Error(`Incorrect array: not only numbers`);
+  });
+};
+
 const hasZeroNum = (arr) => {
-  return arr.some((el) => el === 0);
+  const bool = arr.some((elem) => elem === 0);
+  if (!bool) throw new Error("No 0 in array");
 };
 
 const getSumResult = (arr) => {
   try {
     let sum = 0;
     hasZeroNum(arr);
-    
+    isNumberArray(arr);
+
     for (let elem of arr) {
-      if (elem !== 0) {
-        sum += elem;
-      } else {
-        break;
-      }
+      if (elem === 0) break;
+      sum += elem;
     }
 
     return sum;
   } catch (error) {
-    error.message;
+    return error.message;
   }
 };
 
