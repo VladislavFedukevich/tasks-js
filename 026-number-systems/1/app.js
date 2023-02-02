69a377d147cb3882ccfa14a77d6d197f27623140
karmanchik567 <fedukevicvlad@gmail.com>
2023-02-03 00:29:58 +0300

    fix-1

--- 026-number-systems/1/app.js
+++ 026-number-systems/1/app.js
@@ -1,25 +1,25 @@
-const value = prompt('Enter the value');
+const value = prompt("Enter the value");
 
 const doArray = (val) => {
   let arr = [];
 
   for (let i = 0; i < val; i++) {
-    arr.push(prompt('Enter the item'));
+    arr.push(prompt("Enter the item"));
   }
 
   return arr;
-}
+};
 
 const filteredArray = (arr) => {
   try {
-    const newArr = arr.filter(elem => typeof elem === 'number');
-    if (!newArr.length) throw new Error('Array is empty');
+    const newArr = arr.filter((elem) => typeof elem === "number");
+    if (!newArr.length) throw new Error("Array is empty");
 
     return newArr;
   } catch (e) {
     return e.message;
   }
-}
+};
 
 const array = doArray(value);
 const result = filteredArray(array);
