2061ee13e4303a7e8db9494dce4e4f779d532d54
karmanchik567 <fedukevicvlad@gmail.com>
2023-03-07 23:58:45 +0300

    9

--- 038-repeat/9/app.js
+++ 038-repeat/9/app.js
@@ -1,6 +1,15 @@
 const result = document.querySelector('.container');
 
-const numbers = Array.from({ length: 15 }, () => Math.floor(Math.random() * 101));
+const generate = () => {
+  const arr = [];
+  for (let i = 0; i < 15; i++) {
+    arr.push(Math.floor(Math.random() * 101));
+  }
+
+  return arr;
+}
+
+const numbers = generate();
 
 result.innerHTML = numbers.map((num) => `<div>${num}</div>`).join('');
 
