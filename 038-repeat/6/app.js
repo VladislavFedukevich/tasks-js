ba90874232967798af029000592d0a75ec3c9f05
karmanchik567 <fedukevicvlad@gmail.com>
2023-03-15 23:27:31 +0300

    fix

--- 038-repeat/6/app.js
+++ 038-repeat/6/app.js
@@ -1,20 +1,19 @@
-const btn = document.querySelector('.btn');
-const input = document.querySelector('.input');
-const result = document.querySelector('.result');
+const btn = document.querySelector(".btn");
+const input = document.querySelector(".input");
+const result = document.querySelector(".result");
 const arr = [];
 
-btn.addEventListener('click', () => {
+btn.addEventListener("click", () => {
   try {
-    if(!input.value) throw new Error('Input is empty');
-
+    if (!input.value) throw new Error("Input is empty");
 
     arr.push(input.value);
 
     result.innerHTML = arr;
-    input.value = '';
+    input.value = "";
     input.style = "border: 1px solid black";
   } catch (error) {
     alert(error.message);
     input.style = "border: 1px solid red";
   }
-})
+});
