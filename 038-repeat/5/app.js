dfe4fd4ed0321ec6d73b6b4f14d1db572b027582
karmanchik567 <fedukevicvlad@gmail.com>
2023-03-15 23:27:18 +0300

    fix

--- 038-repeat/5/app.js
+++ 038-repeat/5/app.js
@@ -1,18 +1,18 @@
-const btn = document.querySelector('.btn');
-const input = document.querySelector('.input');
-const result = document.querySelector('.result');
+const btn = document.querySelector(".btn");
+const input = document.querySelector(".input");
+const result = document.querySelector(".result");
 
-btn.addEventListener('click', () => {
+btn.addEventListener("click", () => {
   try {
-    if(isNaN(input.value)) throw new Error('Not a number');
+    if (isNaN(input.value)) throw new Error("Not a number");
 
-    if(input.value > 0) {
+    if (input.value > 0) {
       result.innerHTML = Number(input.value) * 2;
-      input.value = '';
+      input.value = "";
     } else {
-      throw new Error('Less than 0');
+      throw new Error("Less than 0");
     }
   } catch (error) {
     alert(error.message);
   }
-})
+});
