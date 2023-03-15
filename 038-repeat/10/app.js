9a60c0f3262fb5148cbb7ba13c6ee1310bf638da
karmanchik567 <fedukevicvlad@gmail.com>
2023-03-15 23:28:32 +0300

    fix

--- 038-repeat/10/app.js
+++ 038-repeat/10/app.js
@@ -1,18 +1,18 @@
-const input = document.querySelector('input');
-const result = document.querySelector('.result');
-const btn = document.querySelector('button');
+const input = document.querySelector("input");
+const result = document.querySelector(".result");
+const btn = document.querySelector("button");
 const arr = [];
 
-btn.addEventListener('click', () => {
+btn.addEventListener("click", () => {
   try {
-    if (!input.value) throw new Error('Input is empty');
+    if (!input.value) throw new Error("Input is empty");
 
     arr.push(input.value);
     result.innerHTML = `Result: ${arr}`;
-    input.value = '';
+    input.value = "";
   } catch (error) {
     alert(error.message);
     result.innerHTML += `, ${error.message}`;
-    error.message.style = 'color: red';
+    error.message.style = "color: red";
   }
 });
