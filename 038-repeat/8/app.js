80736f2749975c17211b631020bc69a02290f3ee
karmanchik567 <fedukevicvlad@gmail.com>
2023-03-15 23:27:56 +0300

    fix

--- 038-repeat/8/app.js
+++ 038-repeat/8/app.js
@@ -1,7 +1,7 @@
-const list = document.querySelector('.list');
-const item = document.querySelectorAll('li');
-const result = document.querySelector('.result');
+const list = document.querySelector(".list");
+const item = document.querySelectorAll("li");
+const result = document.querySelector(".result");
 
-list.addEventListener('click', (event) => {
-  result.innerHTML += event.target.textContent + ' ';
-})
+list.addEventListener("click", (event) => {
+  result.innerHTML += event.target.textContent + " ";
+});
