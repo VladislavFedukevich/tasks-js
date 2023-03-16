96bb4904bffd83b122774f40d14a50414dcecdf2
karmanchik567 <fedukevicvlad@gmail.com>
2023-03-16 23:14:25 +0300

    fix

--- 038-repeat/10/app.js
+++ 038-repeat/10/app.js
@@ -1,18 +1,30 @@
-const input = document.querySelector("input");
-const result = document.querySelector(".result");
-const btn = document.querySelector("button");
-const arr = [];
+// const input = document.querySelector("input");
+// const result = document.querySelector(".result");
+// const btn = document.querySelector("button");
+// const arr = [];
 
-btn.addEventListener("click", () => {
-  try {
-    if (!input.value) throw new Error("Input is empty");
+// btn.addEventListener("click", () => {
+//   try {
+//     if (!input.value) throw new Error("Input is empty");
 
-    arr.push(input.value);
-    result.innerHTML = `Result: ${arr}`;
-    input.value = "";
-  } catch (error) {
-    alert(error.message);
-    result.innerHTML += `, ${error.message}`;
-    error.message.style = "color: red";
-  }
-});
+//     arr.push(input.value);
+//     result.innerHTML = `Result: ${arr}`;
+//     input.value = "";
+//   } catch (error) {
+//     alert(error.message);
+//     result.innerHTML += `, ${error.message}`;
+//     error.message.style = "color: red";
+//   }
+// });
+
+const getSquare = (num) => {
+  let arr = num.toString().split('');
+  const newArr = [];
+  arr.forEach(elem => {
+    newArr.push((elem * elem));
+  });
+
+  return parseInt(newArr.join(''));
+}
+
+console.log(getSquare(123456));
