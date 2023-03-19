0b95c0a3ec2a69ef5fde399e33bf1431a67c922d
karmanchik567 <fedukevicvlad@gmail.com>
2023-03-19 22:59:39 +0300

    fix

--- 038-repeat/10/app.js
+++ 038-repeat/10/app.js
@@ -1,30 +1,18 @@
-const input = document.querySelector("input");
+const inputVal = document.querySelector("input");
 const result = document.querySelector(".result");
 const btn = document.querySelector("button");
 const arr = [];
 
 btn.addEventListener("click", () => {
   try {
-    if (!input.value) throw new Error("Input is empty");
+    if (!inputVal.value) throw new Error("Input is empty");
 
-    arr.push(input.value);
+    arr.push(inputVal.value);
     result.innerHTML = `Result: ${arr}`;
-    input.value = "";
+    inputVal.value = "";
   } catch (error) {
     alert(error.message);
     result.innerHTML += `, ${error.message}`;
     error.message.style = "color: red";
   }
 });
-
-// const getSquare = (num) => {
-//   let arr = num.toString().split('');
-//   const newArr = [];
-//   arr.forEach(elem => {
-//     newArr.push((elem * elem));
-//   });
-
-//   return parseInt(newArr.join(''));
-// }
-
-// console.log(getSquare(123456));
