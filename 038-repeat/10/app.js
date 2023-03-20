53e1fcb0574a5e56fab3bb2798823c98902c09a1
karmanchik567 <fedukevicvlad@gmail.com>
2023-03-20 23:43:27 +0300

    testing

--- 038-repeat/10/app.js
+++ 038-repeat/10/app.js
@@ -1,18 +1,22 @@
-const inputVal = document.querySelector("input");
-const result = document.querySelector(".result");
-const btn = document.querySelector("button");
-const arr = [];
+// const inputVal = document.querySelector("input");
+// const result = document.querySelector(".result");
+// const btn = document.querySelector("button");
+// const arr = [];
 
-btn.addEventListener("click", () => {
-  try {
-    if (!inputVal.value) throw new Error("Input is empty");
+// btn.addEventListener("click", () => {
+//   try {
+//     if (!inputVal.value) throw new Error("Input is empty");
 
-    arr.push(inputVal.value);
-    result.innerHTML = `Result: ${arr}`;
-    inputVal.value = "";
-  } catch (error) {
-    alert(error.message);
-    result.innerHTML += `, ${error.message}`;
-    error.message.style = "color: red";
-  }
-});
+//     arr.push(inputVal.value);
+//     result.innerHTML = `Result: ${arr}`;
+//     inputVal.value = "";
+//   } catch (error) {
+//     alert(error.message);
+//     result.innerHTML += `, ${error.message}`;
+//     error.message.style = "color: red";
+//   }
+// });
+
+console.log(0 == []);
+console.log(0 === []);
+console.log(0 == '');
