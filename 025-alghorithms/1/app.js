c4c298394230d5fddd995fe57210d4db0895075d
karmanchik567 <fedukevicvlad@gmail.com>
2023-03-21 17:35:18 +0300

    testing

--- 025-alghorithms/1/app.js
+++ 025-alghorithms/1/app.js
@@ -1,25 +1,46 @@
-const number = 3;
-const staticArray = [1, 2, 4, 5, 3];
+// const number = 3;
+// const staticArray = [1, 2, 4, 5, 3];
 
-const searchBinary = (elem, arr) => {
-  let startPosition = 0;
-  let endPosition = arr.length - 1;
+// const searchBinary = (elem, arr) => {
+//   let startPosition = 0;
+//   let endPosition = arr.length - 1;
 
-  for (let i = startPosition; i < endPosition; i++) {
-    let middlePosition = Math.floor((startPosition + endPosition) / 2);
+//   for (let i = startPosition; i < endPosition; i++) {
+//     let middlePosition = Math.floor((startPosition + endPosition) / 2);
 
-    if (arr[middlePosition] === arr[elem]) {
-      console.log(`The result is: ${arr[middlePosition]}`);
-      return elem;
-    } else if (arr[middlePosition] > arr[elem]) {
-      endPosition = middlePosition;
-      continue;
-    } else if (arr[middlePosition] < arr[elem]) {
-      startPosition = middlePosition;
-      continue;
-    }
-  }
+//     if (arr[middlePosition] === arr[elem]) {
+//       console.log(`The result is: ${arr[middlePosition]}`);
+//       return elem;
+//     } else if (arr[middlePosition] > arr[elem]) {
+//       endPosition = middlePosition;
+//       continue;
+//     } else if (arr[middlePosition] < arr[elem]) {
+//       startPosition = middlePosition;
+//       continue;
+//     }
+//   }
+// }
+
+// const result = searchBinary(number, staticArray);
+// console.log(result);
+
+function fetchData(url, options = {}) {
+  return fetch(url, options)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .catch(error => {
+      console.error(`Error fetching data: ${error}`);
+    });
 }
 
-const result = searchBinary(number, staticArray);
-console.log(result);
+const controller = new AbortController();
+
+console.log(fetchData('http://jsonplaceholder.typicode.com/posts', {
+  signal: controller.signal
+}));
+
+controller.abort();
